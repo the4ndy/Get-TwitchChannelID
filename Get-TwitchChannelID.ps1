ac1344d39e5b2af31627273a2b404f4cd47293fe
theSheriff <the4ndy@users.noreply.github.com>
2017-10-23 11:26:48 -0400

    initial commit

---
+++ Get-TwitchChannelID.ps1
@@ -0,0 +1,25 @@
+﻿
+    Param (
+        [Parameter(Mandatory=$True)]
+        [String]$channel
+    )
+
+    $clientID = "sw19g2vrz9nkugaviab9xbk6w7ehxxh"
+    $userAgent = "PowerShell GetChannelID"
+    #$game = [System.Web.HttpUtility]::UrlEncode($gameName);
+    $apiUrl = "https://api.twitch.tv/kraken/users?login="
+    $headers = @{
+        "Client-ID" = $ClientID;
+        "Accept" = "application/vnd.twitchtv.v5+json"
+    }
+    
+    $requestUrl = $apiUrl + $channel
+
+    $apiResult = Invoke-RestMethod -Uri $requestUrl -Method Get -UserAgent $userAgent -Headers $headers
+    if ($apiResult) {
+        return $apiResult.users._id
+    } else {
+        Write-Warning "Sorry, but there was a problem with the last twitch.tv API call."
+        return
+    }
+
